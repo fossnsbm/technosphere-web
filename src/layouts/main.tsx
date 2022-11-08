cf95fb014a1fdc69c52cbf24dea275ef6e84151f
buddhimac111 <91274532+buddhimac111@users.noreply.github.com>
2022-11-09 00:05:01 +0530

    added onclick event on register button

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import HeaderMenus from "../components/headerMenus/headerMenus";
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const theme = createTheme({
@@ -11,8 +12,10 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
   return (
     <>
+      
       <ThemeProvider theme={theme}>
         <CssBaseline />
+        <HeaderMenus />
         <div>{children}</div>
       </ThemeProvider>
     </>
