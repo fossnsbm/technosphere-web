20c2ca264b2942fbf303851859fa375958c4a757
WasathTheekshana <wasath.vt@gmail.com>
2022-11-11 12:22:05 +0530

    feat: IntroSection

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,22 +1,17 @@
 import MainLayout from "./layouts/main";
 import { HomePage } from "./pages/Home";
 import { createBrowserRouter } from "react-router-dom";
-import Profile from "./pages/Profile";
+import { IntroSection } from "./components/IntroSection";
+
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <MainLayout>
-        <HomePage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: "/profile",
-    element: (
-      <MainLayout>
-        <Profile />
+        {/* <HomePage /> */}
+        <IntroSection/>
       </MainLayout>
     ),
   },
