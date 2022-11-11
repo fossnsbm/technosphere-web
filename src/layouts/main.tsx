a7502069854ffa430b1eb2ac2ce6838943c6daa1
Yasith Ranusha <yasithranusha24@gmail.com>
2022-11-11 17:45:23 +0530

    Added home to layout

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Footer from "../components/Footer"
 
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const theme = createTheme({
@@ -14,7 +15,9 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div>{children}</div>
+        <Footer/>
       </ThemeProvider>
+
     </>
   );
 };
