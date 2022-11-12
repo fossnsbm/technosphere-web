c7a978b5048ad618627ad848c412b7450f118030
unknown <rviz@pm.me>
2022-11-12 22:34:28 +0530

    agenda update 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
-import Agenda from "../components/agenda/agenda";
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import Footer from "../components/Footer"
 import { IntroSection } from "../components/IntroSection";
+import Agenda  from "../components/agenda/agenda";
+
 export const HomePage = () => {
   return (
     <>
       <IntroSection />
-      
+      <Agenda />
     </>
   );
 };
