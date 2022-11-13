ee4a06c30aab65142a83a91695d60e1932973120
Thaanu Perera <thaanup@gmail.com>
2022-11-13 21:15:27 +0530

    Add SpeakerSection

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 import Footer from "../components/Footer"
 import { IntroSection } from "../components/IntroSection";
+import SpeakersSection from "../components/speakersSection/speakersSection";
+
 export const HomePage = () => {
   return (
     <>
       <IntroSection />
+      <SpeakersSection/>
     </>
   );
 };
